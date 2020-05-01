15312db43c0d4a12cf81516fc7b141317d37ff9a
Benji Vesterby <benji@devnw.com>
2020-05-01 14:41:23 -0500

    Added new methods for pre-config destinations
    These two new method add the ability to setup alog for testing and
    benchmarking with the standard go test suite.
    
    Resolves GH-12

--- destination.go
+++ destination.go
@@ -3,6 +3,9 @@ package alog
 import (
 	"io"
 	"os"
+	"testing"
+
+	"github.com/pkg/errors"
 )
 
 // Destination is the destination struct for registering io.Writers to the
@@ -32,4 +35,71 @@ func Standards() []Destination {
 			os.Stderr,
 		},
 	}
+}
+
+// TestDestinations returns a list of destinations for logging test data
+// to the *testing.T that was passed in. This defaults all ERROR, CRIT, FATAL
+// logs to the t.Error and the rest are routed to t.Log. These destinations
+// can be used to override the logger with destinations specific to testing.
+func TestDestinations(t *testing.T) []Destination {
+	return []Destination{
+		{
+			INFO | DEBUG | TRACE | WARN | CUSTOM,
+			STD,
+			test{
+				t:    t,
+				mode: INFO | DEBUG | TRACE | WARN | CUSTOM,
+			},
+		},
+		{
+			ERROR | CRIT | FATAL,
+			STD,
+			test{
+				t:    t,
+				mode: ERROR | CRIT | FATAL,
+			},
+		},
+	}
+}
+
+type test struct {
+	t    *testing.T
+	mode LogLevel
+}
+
+func (t test) Write(p []byte) (int, error) {
+	if t.t == nil {
+		return 0, errors.New("invalid test object")
+	}
+
+	msg := string(p)
+	if t.mode&(ERROR|CRIT|FATAL) > 0 {
+		t.t.Error(msg)
+	} else {
+		t.t.Log(msg)
+	}
+
+	return len(p), nil
+}
+
+// BenchDestinations returns a list of destinations for benchmarking.
+// The destination returned from this method does NOTHING. It is meant
+// to remove overhead from the logger for proper benchmarks.
+// This destination can be used to override the logger for benchmarking.
+func BenchDestinations() []Destination {
+	return []Destination{
+		{
+			INFO | DEBUG | TRACE | WARN |
+				CUSTOM | ERROR | CRIT | FATAL,
+			STD,
+			bench{},
+		},
+	}
+}
+
+type bench struct {
+}
+
+func (bench) Write(p []byte) (int, error) {
+	return len(p), nil
 }
